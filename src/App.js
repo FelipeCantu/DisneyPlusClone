cbaa2b1db1e340113059928ebae92059dc47eec5
Felipe <cantujr.felipe@gmail.com>
2021-05-03 10:49:10 -0500

    detail cards

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Login from './components/Login';
-import Header from './components/Header';
-import Home from './components/Home';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Detail from './components/Detail';
 
-import './App.css';
-
+import "./App.css";
 
 function App() {
   return (
@@ -15,8 +15,11 @@ function App() {
           <Route exact path="/">
             <Login />
           </Route>
-          <Route path='/home'>
+          <Route path="/home">
             <Home />
+          </Route>
+          <Route path="/detail/:id">
+            <Detail />
           </Route>
         </Switch>
       </Router>
